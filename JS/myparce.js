e0af6f33b6d126443be996aebafa33182ac5744c
Rodrigo Leal <74040399+rodrigonewgm@users.noreply.github.com>
2022-07-01 22:59:16 -0300

    Update myparce.js
    se almacena el valor de textarea en una variable
    limpiamos la variable
    obtenemos un objeto

--- JS/myparce.js
+++ JS/myparce.js
@@ -1,28 +1,56 @@
-const textarea = document.querySelector("textarea");
+let json ={}
 
 const button = document.querySelector("button");
 button.addEventListener("click", ()=>{
-    console.log(textarea.value())
-})
+    let textarea = document.getElementById("inputText").value;
+    // console.log("hola")
+   
+// vamos a limpiar
+let texareaLimpio = textarea.replace(/['"]+/g,'')
+                            .replace(/,/g, '') //
 
-function parseador() { btn.addEventListener("click", parseador);    } 
+texareaLimpio = texareaLimpio.split('\n') // quito saltos de linea
+texareaLimpio.pop()
+texareaLimpio.shift()
 
-
-const jsonExample = {
-    "first_prop" : "Una cadena de texto",
-    "second_prop" : 125.30,
-    "third_prop" : [
-        {
-            "sub_prop_1" : "Descripción - 1",
-            "sub_prop_2" : 200
-        },
-        {
-            "sub_prop_1" : "Descripción - 2",
-            "sub_prop_2" : 100
-        }
-    ],
-    "forth_prop" : true,
-    "fifth_prop" : null
+for (let i = 0; i < texareaLimpio.length; i++) { // quito espacios
+texareaLimpio[i] = texareaLimpio[i].trim()
+}
+for (let i = 0; i < texareaLimpio.length; i++) { // quito
+texareaLimpio[i] = texareaLimpio[i].split(' : ')
 }
 
-jsonExample.
+
+for (let i = 0; i < texareaLimpio.length; i++) {
+json[texareaLimpio[i][0]] = texareaLimpio[i][1]
+}
+
+if (json.fifth_prop === "null") {
+    json.fifth_prop = null
+} 
+
+if  (json.forth_prop === "true") {
+    json.forth_prop = true
+}
+
+if(typeof json.second_prop === 'string') {
+    json.second_prop = parseInt(json.second_prop)
+    json.sub_prop_2 = parseInt(json.sub_prop_2)
+}
+
+for (let i = 0; i < texareaLimpio.length; i++) {
+if  (texareaLimpio[i][1] === "[") {
+    json.third_prop = []
+    }
+}
+
+for (let i = 0; i < texareaLimpio.length; i++) {
+    if (texareaLimpio[i][1] === "{"){
+
+    }  
+}
+Object.prototype
+console.log(json)
+})
+
+
